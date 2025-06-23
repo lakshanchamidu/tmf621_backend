92a78a9be55c450950d4866ed3bf3b96109b5896
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-06-23 22:42:42 +0530

    Update server.js

--- trouble-ticket-api/server.js
+++ trouble-ticket-api/server.js
@@ -21,7 +21,8 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error:", err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(
     `Server running at http://localhost:${PORT}/tmf-api/troubleTicket/v5/troubleTicket`
